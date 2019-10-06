d7be2ab53a269fbb5dae2a4acb05c74af3b8d851
jcfaieng <55946918+jcfaieng@users.noreply.github.com>
2019-10-06 19:23:00 +0800

    add r packages

--- add_r_pachage.php
+++ add_r_pachage.php
@@ -5,7 +5,26 @@ options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc/")
 options("repos" = c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
 options()$repos 
 options()$BioC_mirror
+
 # https://bioconductor.org/packages/release/bioc/html/GEOquery.html
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("KEGG.db",ask = F,update = F)
+BiocManager::install(c("GSEABase","GSVA","clusterProfiler" ),ask = F,update = F)
+BiocManager::install(c("GEOquery","limma","impute" ),ask = F,update = F)
+BiocManager::install(c("genefu","org.Hs.eg.db","hgu133plus2.db" ),ask = F,update = F)
+
+# 下面代码被我注释了，意思是这些代码不需要运行，因为它过时了，很多旧教程就忽略
+# source("https://bioconductor.org/biocLite.R") 
+# library('BiocInstaller') 
+# options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc/") 
+# BiocInstaller::biocLite("GEOquery")
+# BiocInstaller::biocLite(c("limma"))
+# BiocInstaller::biocLite(c("impute"))
+
+options()$repos
+install.packages('WGCNA')
+install.packages(c("FactoMineR", "factoextra"))
+install.packages(c("ggplot2", "pheatmap","ggpubr"))
+library("FactoMineR")
+library("factoextra")
